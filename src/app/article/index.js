87b02bc0416b28982e89ff6f2f620e9fa1949ad4
edoner <edonet@163.com>
2018-05-17 23:54:58 +0800

    update: article

--- src/app/article/index.js
+++ src/app/article/index.js
@@ -14,6 +14,7 @@
  */
 import React, { Component } from 'react';
 import { use } from 'ylan/style';
+import { fetchArticle } from '../../api';
 import style from './index.scss';
 
 
@@ -22,7 +23,9 @@ import style from './index.scss';
  * 定义变量
  *****************************************
  */
-const styled = use(style);
+const
+    styled = use(style),
+    articleCache = {};
 
 
 /**
@@ -31,16 +34,85 @@ const styled = use(style);
  *****************************************
  */
 export default class AppArticle extends Component {
+
+    /* 初始化组件 */
+    constructor(props, ...args) {
+        super(props, ...args);
+
+        // 定义状态
+        this.state = { id: '', code: '' };
+    }
+
+     /* 更新状态 */
+    static getDerivedStateFromProps(props, state) {
+
+        // 更新状态
+        if (props.article) {
+            if (props.article.id !== state.id) {
+                return { id: props.article.id, code: '' };
+            }
+        } else if (state.id) {
+            return { id: '', code: '' };
+        }
+
+        // 不变
+        return null;
+    }
+
+    /* 渲染组件 */
     render() {
-        let { article } = this.props;
 
         // 返回元素
         return (
             <div className={ styled('container flex tc') }>
                 <article className={ styled('article ph20 pt30 tl') }>
-                    { article && article.title }
+                    { this.state.code }
                 </article>
             </div>
         );
+    }
+
+    /* 挂载完成 */
+    componentDidMount() {
+        this.componentDidUpdate(null, {});
+    }
+
+    /* 更新完成 */
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.id !== this.state.id) {
+            this.fetchArticleContent(this.state.id);
+        }
+    }
+
+    /* 获取文档内容 */
+    async fetchArticleContent(id) {
+
+        // 获取内容
+        if (id) {
+
+            // 创建缓存
+            if (!(id in articleCache)) {
+                try {
+                    let res = await fetchArticle(id);
+
+
+                    // 处理错误返回
+                    if (res.status !== 200) {
+                        throw new Error('加载文件失败，请稍后再试');
+                    }
+
+                    // 缓存内容
+                    articleCache[id] = res.data;
+                } catch (err) {
+                    articleCache[id] = err.message || '加载文件失败，请稍后再试';
+                }
+            }
+
+            // 更新内容
+            return this.setState({ id, code: articleCache[id] });
+        }
+
+        // 清空文档
+        this.setState({ id: '', code: '' });
     }
 }
